96b95311b309d23e0ce6293e1db60b5d86a37b36
AllAboutAzure <156210181+AllAboutAzure@users.noreply.github.com>
2024-01-19 11:57:11 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,5 +8,12 @@ terraform {
 }
 
 provider "azurerm" {
-  # Configuration options
+  # Configuration options  
+  # these variables can be configured in the Terraform CLOUD 
+  # the mentioned variables are the information related to service principal which is created in Azure Cloud for deploying terraform
+  # az ad sp create-for-rbac --role='Contributor' --scopes = '<<subscription Scope>>' --> Run this cmd in azure cli to generate spn
+  subscription_id = var.subscriptionID
+  client_id= var.clientID
+  client_secret = var.clientSecret
+  tenant_id = var.tenantID
 }
