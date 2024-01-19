2bb34cf0984465980254a2ce43a5c5c2fc304fdf
AllAboutAzure <156210181+AllAboutAzure@users.noreply.github.com>
2024-01-19 10:54:42 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -1,9 +1,3 @@
-# pylint: disable=missing-module-docstring
-# pylint: disable=missing-class-docstring
-# pylint: disable=missing-function-docstring
-# pylint: disable=invalid-name
-# pylint: disable=trailing-newlines
-# pylint: disable=trailing-whitespace
-# pylint: disable=missing-final-newline
+"""Module has a simple print statement"""
 
 print("Hello World!! Settings")
