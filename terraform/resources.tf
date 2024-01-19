21d4f36cc874f1784b8e1c6d1296a58263c8b21d
AllAboutAzure <156210181+AllAboutAzure@users.noreply.github.com>
2024-01-19 11:29:24 +0530

    Update resources.tf

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,1 +1,16 @@
+#### Resource Group ####
+resource "azurerm_resource_group" "resource-group" {
+  name     = "testing-rg"
+  location = "West Europe"
+}
 
+#### Automation Account ####
+resource "azurerm_automation_account" "automation_account" {
+  name                = "aa-test-automation-account"
+  location            = azurerm_resource_group.resource-group.location
+  resource_group_name = azurerm_resource_group.resource-group.name
+  sku_name            = "Basic"
+  tags = {
+    environment = "development"
+  }
+}
