44195ecd43dd335375bca98a05347cf703ab9ee7
AllAboutAzure <156210181+AllAboutAzure@users.noreply.github.com>
2024-01-19 11:39:59 +0530

    Update resources.tf

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -14,3 +14,17 @@ resource "azurerm_automation_account" "automation_account" {
     environment = "development"
   }
 }
+
+#### Automation Runbooks ####
+resource "azurerm_automation_runbook" "aa-runbooks" {
+  for_each = {for inst in runbooks: inst.name => inst}
+  name                    = each.value.name
+  location                = azurerm_resource_group.resource-group.location
+  resource_group_name     = azurerm_resource_group.resource-group.name
+  automation_account_name = azurerm_automation_account.automation_account.name
+  log_verbose             = "true"
+  log_progress            = "true"
+  description             = each.value.description
+  runbook_type            = each.value.runbook_type
+  content                 = file(each.value.file_content)
+}
