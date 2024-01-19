c8bdc90f72454e8add8fa00cad2f6bd13e002e83
AllAboutAzure <156210181+AllAboutAzure@users.noreply.github.com>
2024-01-19 11:44:20 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,5 +1,5 @@
 output "runbooks-created" {
-  value = { for val in  : val.name => {
+  value = { for val in azurerm_automation_runbook.aa-runbooks : val.name => {
     id      = val.id
     name    = val.name
     runbookType = val.runbook_type
