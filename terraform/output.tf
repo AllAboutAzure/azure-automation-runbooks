fa57ee83e3603922cd3181b248a55ae7b75f5a45
AllAboutAzure <156210181+AllAboutAzure@users.noreply.github.com>
2024-01-19 11:43:38 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,1 +1,8 @@
-
+output "runbooks-created" {
+  value = { for val in  : val.name => {
+    id      = val.id
+    name    = val.name
+    runbookType = val.runbook_type
+  } }
+  descriptions = "details of the runbooks created"
+}
