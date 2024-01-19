b692467b1609761d36fde43e04a5bb83f211db87
AllAboutAzure <156210181+AllAboutAzure@users.noreply.github.com>
2024-01-19 10:51:19 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -1,1 +1,9 @@
-print("hello World")
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=invalid-name
+# pylint: disable=trailing-newlines
+# pylint: disable=trailing-whitespace
+# pylint: disable=missing-final-newline
+
+print("Hello World!! Settings")
