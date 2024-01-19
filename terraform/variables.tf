16ec0ab5cfb619e44c6bce216a68e5b1e9426a07
AllAboutAzure <156210181+AllAboutAzure@users.noreply.github.com>
2024-01-19 11:59:45 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,20 @@
 variable "runbooks" {
-  name = "name of the runbooks"
+  description = "name of the runbooks"
   default = {}
+}
+
+variable "subscriptionID" {
+  type = string
+}
+
+variable "clientID" {
+  type = string
+}
+
+variable "clientSecret" {
+  type = string
+}
+
+variable "tenantID" {
+  type = string
 }
